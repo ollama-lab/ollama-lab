68731e26e0c9d1cfeca50e30fee708d7b2e6488e
Charles Dong <chardon_cs@proton.me>
2024-06-29 04:06:21 +0800

    Move database management to Tauri backend

--- src/constants.ts
+++
@@ -1,1 +0,0 @@
-export const DEFAULT_DB_PATH = process.platform === "win32" ? "~/Documents/ollama-lab/db.sqlite" : "~/.config/ollama-lab/db.sqlite";
--- src/lib/db.ts
+++
@@ -1,5 +0,0 @@
-import { drizzle } from "drizzle-orm/better-sqlite3";
-import Database from 'better-sqlite3';
-import { DEFAULT_DB_PATH } from "../constants";
-
-export const db = drizzle(new Database(DEFAULT_DB_PATH));
