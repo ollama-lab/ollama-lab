f1271a55a0d2f2b4c1fc450ba1534321752d9654
Charles Dong <chardon_cs@proton.me>
2024-07-02 14:28:35 +0800

    Use static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import { preprocessMeltUI } from '@melt-ui/pp';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
