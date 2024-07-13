90df398bafb84c5bec47bab3916b22b37a333a1f
Charles Dong <chardon_cs@proton.me>
2024-07-13 23:59:17 +0800

    Add settings tab

--- src/lib/components/sector.svelte
+++ src/lib/components/sector.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { AppRail, AppRailTile } from "@skeletonlabs/skeleton";
-  import { IconBrain, IconMessageChatbot } from "@tabler/icons-svelte"
+  import { IconBrain, IconMessageChatbot, IconSettings } from "@tabler/icons-svelte"
 
   const tabData = [
     {
@@ -12,6 +12,14 @@
       name: "models",
       label: "Models",
       icon: IconBrain,
+    },
+  ]
+
+  const trailTabData = [
+    {
+      name: "settings",
+      label: "Settings",
+      icon: IconSettings,
     },
   ]
 
@@ -28,4 +36,12 @@
       <span>{label}</span>
     </AppRailTile>
   {/each}
+
+  <svelte:fragment slot="trail">
+    {#each trailTabData as { name, label, icon } (name)}
+      <AppRailTile bind:group={currentTile} {name} value={name} title={label}>
+        <svelte:component this={icon} class="mx-auto" />
+      </AppRailTile>
+    {/each}
+  </svelte:fragment>
 </AppRail>
