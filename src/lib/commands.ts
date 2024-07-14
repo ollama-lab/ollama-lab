4f536b15e1de15cf832482879e631f7732168f97
Charles Dong <chardon_cs@proton.me>
2024-07-14 22:24:50 +0800

    Add Tauri port

---
+++ src/lib/commands.ts
@@ -0,0 +1,4 @@
+// Reexport invoke for future convenience
+//
+// If we want to port this frontend to backend other than Tauri, we can just update this file (theoretically)
+export { invoke } from "@tauri-apps/api/tauri"
