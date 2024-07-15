c367cbdb5db3105075641b0d33757aba972a050d
Charles Dong <chardon_cs@proton.me>
2024-07-15 12:33:17 +0800

    Create workspace / add crates

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,12 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+mod streaming;
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .register_uri_scheme_protocol("ollama", |app, req| {
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
