9b279d1fa1aa49b150a02e1cb8b9ef68393de2ee
Charles Dong <chardon_cs@proton.me>
2024-07-15 13:03:09 +0800

    Update dependencies

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,11 +1,11 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-mod streaming;
-
 fn main() {
     tauri::Builder::default()
-        .register_uri_scheme_protocol("ollama", |app, req| {
+        .setup(|app| {
+            // TODO
+            Ok(())
         })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
