7b766e9df28c4fe6fc78b32ece3e7297b2d42401
Charles Dong <chardon_cs@proton.me>
2024-07-30 22:11:43 +0800

    Add bubble models

--- db-desktop/src/model.rs
+++ db-desktop/src/model.rs
@@ -1,2 +1,36 @@
+use std::{fmt::Display, str::FromStr};
+
+pub mod bubble;
 pub mod user;
 pub mod session;
+
+pub struct RoleConversionError;
+
+pub enum Role {
+    System,
+    User,
+    Assistant,
+}
+
+impl FromStr for Role {
+    type Err = RoleConversionError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(match s {
+            "system" => Role::System,
+            "user" => Role::User,
+            "assistant" => Role::Assistant,
+            _ => Err(RoleConversionError)?,
+        })
+    }
+}
+
+impl Display for Role {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", match self {
+            Role::System => "system",
+            Role::User => "user",
+            Role::Assistant => "assistant",
+        })
+    }
+}
---
+++ db-desktop/src/model/bubble.rs
@@ -0,0 +1,62 @@
+use std::{intrinsics::unreachable, str::FromStr};
+
+use chrono::{DateTime, Local};
+use diesel::prelude::*;
+
+use crate::schema::bubbles;
+
+use super::Role;
+
+#[derive(Debug, Queryable, Selectable)]
+pub struct Bubble {
+    id: i32,
+    session: i32,
+    role: String,
+    content: String,
+    date_created: DateTime<Local>,
+    is_edited: bool,
+}
+
+impl Bubble {
+    #[inline]
+    pub fn id(&self) -> i32 {
+        self.id
+    }
+
+    #[inline]
+    pub fn session(&self) -> i32 {
+        self.session
+    }
+
+    #[inline]
+    pub fn role(&self) -> Role {
+        Role::from_str(self.role.as_str())
+            .unwrap_or_else(|_| unreachable!())
+    }
+
+    #[inline]
+    pub fn content(&self) -> &str {
+        self.content.as_str()
+    }
+
+    #[inline]
+    pub fn date_created(&self) -> &DateTime<Local> {
+        &self.date_created
+    }
+}
+
+#[derive(Debug, Insertable)]
+#[diesel(table_name = bubbles)]
+pub struct NewBubble<'a> {
+    session: i32,
+    role: String,
+    content: &'a str,
+}
+
+impl<'a> NewBubble<'a> {
+    #[must_use]
+    #[inline]
+    pub fn new(session: i32, role: Role, content: &'a str) -> Self {
+        Self { session, role: role.to_string(), content }
+    }
+}
