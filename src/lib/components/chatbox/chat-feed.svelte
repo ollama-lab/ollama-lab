8ef0be17ac2e878f90c6073b8bbc4e46f6e95bf9
Charles Dong <chardon_cs@proton.me>
2024-08-11 17:00:48 +0800

    Use local state

--- src/lib/components/chatbox/chat-feed.svelte
+++ src/lib/components/chatbox/chat-feed.svelte
@@ -4,12 +4,14 @@
   import PromptEntry from "./prompt-entry.svelte";
   import Welcome from "./chat-feed/welcome.svelte";
   import BubbleList from "./bubble-list.svelte";
-  import { currentSession } from "$lib/stores/sessions";
+  import { currentSession, sessionList } from "$lib/stores/sessions";
+
+  let currentModel: string | undefined = $sessionList.find(s => s.id === $currentSession)?.currentModel
 </script>
 
 <div class="w-full h-dvh flex flex-col">
   <AppBar class="shadow-sm z-[1]">
-    <ModelSelector />
+    <ModelSelector bind:currentModel />
   </AppBar>
   <div class="flex-auto overflow-y-scroll">
     {#if $currentSession === null}
