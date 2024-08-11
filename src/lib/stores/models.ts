64b237f56fca019f1bd4e49b7967ac19faef099d
Charles Dong <chardon_cs@proton.me>
2024-08-11 17:01:08 +0800

    Remove current model store / use readonly front

--- src/lib/stores/models.ts
+++ src/lib/stores/models.ts
@@ -1,16 +1,17 @@
 import type { Model, RunningModel } from "$lib/models/models"
-import { derived, readable, writable } from "svelte/store"
-import { currentSession, sessionList } from "./sessions"
+import { readable, readonly, writable } from "svelte/store"
 import { listModels } from "$lib/utils/commands"
 
-export const modelList = writable<Model[]>([], set => {
+const internalModelList = writable<Model[]>([], set => {
   listModels()
     .then(models => set(models))
     .catch(err => console.error(err))
 })
 
-export const runningModels = readable<RunningModel[]>([])
+export const modelList = readonly(internalModelList)
 
-export const currentModel = derived([modelList, sessionList, currentSession], ([$m, $s, $cs]) => {
-  return $m.length ? $m[$cs ? ($s.find(item => item.id === $cs)?.id ?? 0) : 0] : null
-})
+export async function refreshModelList() {
+  internalModelList.set(await listModels())
+}
+
+export const runningModels = readable<RunningModel[]>([])
