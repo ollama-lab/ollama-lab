0b70248a2ea05c87bd02b314dcafa07e7c6a5311
Charles Dong <chardon_cs@proton.me>
2024-08-11 14:51:47 +0800

    Update config

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,29 @@
+use command::{chat::{list_chat_bubbles, regenerate, send_prompt}, model::list_models, session::{list_sessions, remove_session}};
+use db::auto_load_db_url;
+
+mod api;
+mod command;
+mod db;
+mod error;
+mod paths;
+mod settings;
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![
+            list_chat_bubbles,
+            list_models,
+            list_sessions,
+            regenerate,
+            remove_session,
+            send_prompt,
+        ])
+        .setup(|_| {
+            auto_load_db_url().unwrap();
+
+            Ok(())
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,28 +1,6 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use command::{chat::{list_chat_bubbles, regenerate, send_prompt}, model::list_models, session::{list_sessions, remove_session}};
-use db::auto_load_db_url;
-
-mod api;
-mod command;
-mod db;
-mod error;
-mod paths;
-mod settings;
-
 fn main() {
-    auto_load_db_url().unwrap();
-
-    tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![
-            list_chat_bubbles,
-            list_models,
-            list_sessions,
-            regenerate,
-            remove_session,
-            send_prompt,
-        ])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+    ollama_lab_lib::run()
 }
