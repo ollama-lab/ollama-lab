5a67f18f87a2b6e1950b7221155848c3f8d56563
Charles Dong <chardon_cs@proton.me>
2025-01-07 09:26:24 +0800

    Update indentation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-	import '../app.css';
-    import { ModeWatcher } from "mode-watcher"
+  import '../app.css';
+  import { ModeWatcher } from "mode-watcher"
 
-	let { children } = $props();
+  let { children } = $props();
 </script>
 
 <ModeWatcher defaultTheme="system" />
