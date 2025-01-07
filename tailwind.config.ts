d1eb359e1e4b3edad5b4cae303011b30ccaa2783
Charles Dong <chardon_cs@proton.me>
2025-01-07 15:22:28 +0800

    Use Inter font by default

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -67,7 +67,7 @@ const config: Config = {
 				sm: "calc(var(--radius) - 4px)"
 			},
 			fontFamily: {
-				sans: ["Figtree Variable", ...fontFamily.sans]
+				sans: ["Inter Variable", ...fontFamily.sans]
 			},
 			keyframes: {
 				"accordion-down": {
