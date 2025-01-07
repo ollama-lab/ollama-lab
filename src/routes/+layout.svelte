f52d074126b08678ac3a1850017eb3886611af5e
Charles Dong <chardon_cs@proton.me>
2025-01-07 09:01:33 +0800

    Add components

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
 	import '../app.css';
+    import { ModeWatcher } from "mode-watcher"
+
 	let { children } = $props();
 </script>
+
+<ModeWatcher defaultTheme="system" />
 
 {@render children()}
