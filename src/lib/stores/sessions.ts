baf24bdf694e5c6cf7c8296dc4568a672fe2c02f
Charles Dong <chardon_cs@proton.me>
2025-01-08 16:13:26 +0800

    Remove chat history store

--- src/lib/stores/sessions.ts
+++ src/lib/stores/sessions.ts
@@ -1,14 +1,6 @@
-import type { ChatHistory, Session } from "$lib/models/session"
-import { derived, writable } from "svelte/store"
+import type { Session } from "$lib/models/session"
+import { writable } from "svelte/store"
 
 export const currentSessionId = writable<number | undefined>()
 
 export const sessions = writable<Session[] | undefined>()
-
-export const chatHistory = derived([sessions, currentSessionId], ([$s, $c], set) => {
-  // TODO: Add logic
-  set({
-    sessionId: 1,
-    bubbles: [],
-  } satisfies ChatHistory)
-})
