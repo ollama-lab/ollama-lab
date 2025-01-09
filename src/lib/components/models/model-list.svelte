5b5a8a28c53f5e8cd93681fda757a2f3e6928f3d
Charles Dong <chardon_cs@proton.me>
2025-01-09 16:10:12 +0800

    Add model list component

---
+++ src/lib/components/models/model-list.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import { CloudDownloadIcon } from "lucide-svelte"
+  import Button from "../ui/button/button.svelte"
+  import { ScrollArea } from "../ui/scroll-area"
+
+</script>
+
+<div class="w-full h-full flex flex-col">
+  <div class="py-4 px-3 flex-shrink-0 flex place-items-center">
+    <h2 class="text-lg font-bold select-none flex-grow">Models</h2>
+    <div class="flex-shrink-0">
+      <Button
+        size="icon"
+        variant="outline"
+        title="Pull model"
+        onclick={() => {
+        }}
+      >
+        <CloudDownloadIcon />
+      </Button>
+    </div>
+  </div>
+  <ScrollArea class="flex-grow">
+    <div class="flex flex-col gap-2 px-2">
+    </div>
+  </ScrollArea>
+</div>
--- src/routes/models/+page.svelte
+++ src/routes/models/+page.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
   import { PlaceholderTitle } from "$lib/components/models"
+  import ModelList from "$lib/components/models/model-list.svelte"
   import { ResizablePane, ResizablePaneGroup } from "$lib/components/ui/resizable"
   import ResizableHandle from "$lib/components/ui/resizable/resizable-handle.svelte"
 </script>
 
 <ResizablePaneGroup direction="horizontal">
   <ResizablePane defaultSize={25}>
+    <ModelList />
   </ResizablePane>
   <ResizableHandle />
   <ResizablePane>
