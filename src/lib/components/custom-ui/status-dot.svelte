4fb3c3d11ac0e1c0b8ab79728ef97a23fcda9a44
Charles Dong <chardon_cs@proton.me>
2025-01-10 10:25:31 +0800

    Add status dot component

---
+++ src/lib/components/custom-ui/status-dot.svelte
@@ -0,0 +1,20 @@
+<script lang="ts" module>
+  export type DotStatus = "success" | "warning" | "error" | "disabled"
+</script>
+
+<script lang="ts">
+  import { cn } from "$lib/utils"
+  import { DotIcon } from "lucide-svelte"
+
+  let { status }: { status: DotStatus } = $props()
+</script>
+
+<DotIcon
+  class={cn(
+    "size-10 -m-2",
+    status === "success" && "text-green-600",
+    status === "warning" && "text-yellow-600",
+    status === "error" && "text-red-600",
+    status === "disabled" && "text-gray-600",
+  )}
+/>
