fe216db5cb1df1a129e643caf039931edd3faadc
Charles Dong <chardon_cs@proton.me>
2025-01-15 15:21:42 +0800

    Update database file initialization

--- src-tauri/src/commands/init.rs
+++ src-tauri/src/commands/init.rs
@@ -1,21 +1,38 @@
-use std::{ops::Deref, sync::Arc};
+use std::{io::ErrorKind, ops::Deref, sync::Arc};
 
 use sqlx::SqlitePool;
 use tauri::State;
 
-use crate::{app_state::AppState, errors::Error, paths::db_path};
+use crate::{app_state::AppState, errors::Error, paths::{db_path, local_data_dir}};
 
 #[tauri::command]
 pub async fn initialize(state: State<'_, Arc<AppState>>) -> Result<(), Error> {
     let mut cur_conn = state.deref().conn.lock().await;
 
     if cur_conn.is_none() {
-        *cur_conn = Some(SqlitePool::connect(
+        tokio::fs::create_dir_all(local_data_dir().ok_or(Error::NoDataPath)?).await?;
+        match tokio::fs::File::create_new(db_path().ok_or(Error::NoDataPath)?).await {
+            Ok(_) => {}
+            Err(err) => {
+                match err.kind() {
+                    ErrorKind::AlreadyExists => {}
+                    _ => Err(err)?,
+                }
+            }
+        }
+
+        let conn = SqlitePool::connect(
             db_path()
                 .as_ref()
                 .and_then(|path| path.to_str())
                 .ok_or(Error::NoDataPath)?
-        ).await?);
+        ).await?;
+
+        sqlx::migrate!("../migrations/")
+            .run(&conn)
+            .await?;
+
+        *cur_conn = Some(conn);
     }
 
     Ok(())
