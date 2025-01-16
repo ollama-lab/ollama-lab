0f0c2ecd96a9ac0d7125dfccdd07fa5eb64dfb8f
Charles Dong <chardon_cs@proton.me>
2025-01-16 16:09:31 +0800

    Add getting default model to initialization

--- src/lib/commands/init.ts
+++ src/lib/commands/init.ts
@@ -1,5 +1,9 @@
 import { invoke } from "@tauri-apps/api/core"
+import { defaultModel } from "$lib/stores/models"
 
 export async function initialize(): Promise<void> {
   await invoke("initialize")
+
+  // Misc
+  defaultModel.set(await invoke("get_default_model"))
 }
