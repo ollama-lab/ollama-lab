549fdf3fcadc67612ce15db797405f0cadba0fb5
Charles Dong <chardon_cs@proton.me>
2025-01-26 17:36:58 +0800

    Update chat history store

--- src/lib/stores/chats.ts
+++ src/lib/stores/chats.ts
@@ -1,23 +1,38 @@
+import { getCurrentBranch } from "$lib/commands/chat-history"
 import type { ChatBubble } from "$lib/models/session"
 import { writable } from "svelte/store"
 
 export interface ChatHistory {
   session: number
   chats: ChatBubble[]
+  loading: boolean
 }
 
 const internalChatHistory = writable<ChatHistory | undefined>()
 
 export const chatHistory = {
   subscribe: internalChatHistory.subscribe,
-  async setSessionId(sessionId: number | null) {
+  async setSessionId(sessionId: number | null): Promise<void> {
     if (sessionId === null) {
       internalChatHistory.set(undefined)
       return
     }
+
+    internalChatHistory.set({
+      session: sessionId,
+      chats: [],
+      loading: true,
+    })
+
+    const chats = await getCurrentBranch(sessionId)
+    internalChatHistory.set({
+      session: sessionId,
+      chats,
+      loading: false,
+    })
   },
-  async clear() {
-    return this.setSessionId(null)
+  async clear(): Promise<void> {
+    await this.setSessionId(null)
   },
   async submit() {
   },
