65bf38e6a2738ed02115f2bca972659077490e4b
Charles Dong <chardon_cs@proton.me>
2025-02-07 12:35:52 +0800

    Remove copy button for codeblocks

--- src/lib/markdown/index.ts
+++ src/lib/markdown/index.ts
@@ -34,7 +34,6 @@ renderer.code = ({ text, lang }) => {
       <div class="header">
         <span class="code-lang">${lang}</span>
         <div class="toolbar">
-          <button class="copy"><svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-copy"><rect width="14" height="14" x="8" y="8" rx="2" ry="2"/><path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2"/></svg></button>
         </div>
       </div>
       <pre class="code-container"><code class="${codeClass}">${text}</code></pre>
