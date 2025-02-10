d2322dac4488c32a6d4df8099e2aeb128e2c59dc
Charles Dong <chardon_cs@proton.me>
2025-02-10 11:59:26 +0800

    Add system prompt textarea

---
+++ src/lib/components/models/model-details/system-prompt.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { Textarea } from "$lib/components/ui/textarea"
+
+</script>
+
+<div class="flex flex-col px-2 py-2 gap-2">
+  <div class="text-sm text-muted-foreground">
+    <p>Tell the model how to behave. System prompts in existing sessions will not be changed.</p>
+  </div>
+
+  <Textarea>
+  </Textarea>
+</div>
