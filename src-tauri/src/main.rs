0795dec63ec225dd597a53a7c48701b757a6f609
Charles Dong <chardon_cs@proton.me>
2025-02-14 12:50:46 +0800

    Add basic CLI

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,12 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use clap::Parser;
+
+mod cli;
+
 fn main() {
+    let _ = cli::Args::parse();
+
     ollama_lab_lib::run()
 }
