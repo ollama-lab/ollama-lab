523322e296a4603c3848beea6e2a89f69bed1f26
Charles Dong <chardon_cs@proton.me>
2025-02-25 17:03:49 +0800

    Update table

--- migrations/20250225030944_add-image-table.sql
+++ migrations/20250225030944_add-image-table.sql
@@ -1,7 +1,6 @@
 CREATE TABLE prompt_images (
     id          INTEGER NOT NULL PRIMARY KEY,
     chat_id     INTEGER NOT NULL REFERENCES chats (id) ON DELETE CASCADE ON UPDATE CASCADE,
-    path        TEXT,
-    image       BLOB,
-    mime        TEXT 
+    origin      TEXT,
+    image       BLOB NOT NULL
 );
