8257d9632480bb7a97a0c143b04d278a2c0b4a2a
Charles Dong <chardon_cs@proton.me>
2025-03-01 00:47:45 +0800

    Initialize Vite Solid project

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite'
+import solid from 'vite-plugin-solid'
+
+const host = process.env.TAURI_DEV_HOST
+
+export default defineConfig({
+  clearScreen: false,
+  server: {
+    strictPort: true,
+    host: host || false,
+    port: 5173,
+  },
+  envPrefix: ['VITE_', 'TAURI_ENV_*'],
+  build: {
+    // Tauri uses Chromium on Windows and WebKit on macOS and Linux
+    target:
+      process.env.TAURI_ENV_PLATFORM == 'windows'
+        ? 'chrome105'
+        : 'safari13',
+    // don't minify for debug builds
+    minify: !process.env.TAURI_ENV_DEBUG ? 'esbuild' : false,
+    // produce sourcemaps for debug builds
+    sourcemap: !!process.env.TAURI_ENV_DEBUG,
+  },
+  plugins: [solid()],
+})
