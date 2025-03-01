e3caab0d2519ba867a5c147399201179c1ae8bcd
Charles Dong <chardon_cs@proton.me>
2025-03-01 19:20:16 +0800

    Add color mode provider

--- src/layout.tsx
+++ src/layout.tsx
@@ -4,17 +4,21 @@ import "@fontsource-variable/inter/wght.css";
 import { AppBar } from "./lib/components/app-bar";
 import { children } from "solid-js";
 import { SettingsProvider } from "./lib/contexts/settings";
+import { ColorModeProvider } from "./lib/components/color-mode/provider";
 
 export function Layout(props: { children?: JSX.Element }) {
   const childrenComp = children(() => props.children);
 
   return (
     <SettingsProvider>
-      <div class="flex flex-row w-dvw h-dvh">
-        <AppBar />
+      {/* Must behind settings provider */}
+      <ColorModeProvider>
+        <div class="flex flex-row w-dvw h-dvh">
+          <AppBar />
 
-        <div class="grow">{childrenComp()}</div>
-      </div>
+          <div class="grow">{childrenComp()}</div>
+        </div>
+      </ColorModeProvider>
     </SettingsProvider>
   );
 }
