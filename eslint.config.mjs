38de29940637cdee11817d371c1a134560fac6c5
Charles Dong <chardon_cs@proton.me>
2025-03-04 10:37:42 +0800

    Update packages

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,10 +3,19 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import eslintPluginSolid from "eslint-plugin-solid";
+import { includeIgnoreFile } from "@eslint/compat";
+// @ts-ignore Works for Node.js and Bun
+import { resolve } from 'node:path';
+
+const gitignorePath = resolve(import.meta.dirname, ".gitignore");
 
 export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.strict,
   tseslint.configs.stylistic,
   eslintPluginSolid.configs['flat/typescript'],
+  includeIgnoreFile(gitignorePath),
+  {
+    ignores: ["src-svelte/"],
+  },
 );
