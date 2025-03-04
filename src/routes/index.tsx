b28d17aa22187254755f62264c9d447971e6ea1b
Charles Dong <chardon_cs@proton.me>
2025-03-04 22:22:14 +0800

    Add session list

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,11 @@
+import { SessionList } from "~/lib/components/chat-sessions/session-list";
 import { Resizable, ResizablePanel } from "~/lib/components/ui/resizable";
 
 export default function IndexPage() {
   return (
     <Resizable orientation="horizontal">
       <ResizablePanel initialSize={.25} collapsible>
+        <SessionList />
       </ResizablePanel>
       <ResizablePanel />
       <ResizablePanel>
