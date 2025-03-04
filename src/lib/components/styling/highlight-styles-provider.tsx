d0681a54f0cc3e363007656cb67b4a0ecd737ed3
Charles Dong <chardon_cs@proton.me>
2025-03-04 19:30:53 +0800

    Update dark mode code color

--- src/lib/components/styling/highlight-styles-provider.tsx
+++ src/lib/components/styling/highlight-styles-provider.tsx
@@ -1,6 +1,6 @@
 import { JSX } from "solid-js";
 import lightModeCSS from "highlight.js/styles/atom-one-light.min.css?raw";
-import darkModeCSS from "highlight.js/styles/atom-one-dark-reasonable.min.css?raw";
+import darkModeCSS from "highlight.js/styles/tokyo-night-dark.min.css?raw";
 import { Style } from "@solidjs/meta";
 import { preferredColorMode } from "~/lib/contexts/globals/color-mode";
 
