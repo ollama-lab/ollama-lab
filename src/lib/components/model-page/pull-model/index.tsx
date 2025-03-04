f12716001b394019952cabbbb3266be62ad26524
Charles Dong <chardon_cs@proton.me>
2025-03-04 16:28:18 +0800

    Add transition and update pull model command

---
+++ src/lib/components/model-page/pull-model/index.tsx
@@ -0,0 +1,153 @@
+import { createMemo, createSignal, Match, Show, Switch } from "solid-js";
+import { Button } from "~/lib/components/ui/button";
+import { CloudDownloadIcon } from "lucide-solid";
+import { CommandDialog, CommandGroup, CommandInput, CommandList } from "~/lib/components/ui/command";
+import { BASE_DOMAIN, BASE_URL, useModelSearchResult } from "~/lib/contexts/model-search-result";
+import { openUrl } from "@tauri-apps/plugin-opener";
+import { Motion, Presence } from "solid-motionone";
+import { platform } from "@tauri-apps/plugin-os";
+import { LoaderSpin } from "../../loader-spin";
+
+interface HintsProps {
+  searchEntered?: boolean;
+}
+
+function Hints(props: HintsProps) {
+  const searchEntered = () => props.searchEntered;
+
+  const os = createMemo(() => platform());
+
+  return (
+    <Show when={searchEntered()}>
+      <Motion.div
+        class="flex gap-2 md:gap-4 lg:gap-6 text-xs px-2 py-2"
+        transition={{ duration: 300 }}
+        initial={{ x: -100, opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        exit={{ x: -100, opacity: 1 }}
+      >
+        <span class="flex gap-2 items-center">
+          <span class="z-[1]"><kbd>Enter</kbd></span>
+          Search
+        </span>
+        <span class="flex gap-2 items-center">
+          <span>
+            <Switch fallback={<><kbd>Ctrl</kbd> + <kbd>Enter</kbd></>}>
+              <Match when={os() === "macos" || os() === "ios"}>
+                <kbd>{"\u2318" /* Command key symbol */} Enter</kbd>
+              </Match>
+            </Switch>
+          </span>
+          <span>Pull entered anyway</span>
+        </span>
+      </Motion.div>
+    </Show>
+  );
+}
+
+export function PullModel() {
+  const searchResultContext = useModelSearchResult();
+
+  const keyword = () => searchResultContext?.keyword() ?? "";
+  const searchResult = () => searchResultContext?.searchResult;
+  const initiateSearch = (keyword: string) => searchResultContext?.initiateSearch(keyword);
+
+  const [open, setOpen] = createSignal(false);
+
+  const [inputKeyword, setInputKeyword] = createSignal<string>(keyword());
+
+  return (
+    <>
+      <Button
+        size="icon"
+        variant="outline"
+        title="Pull model"
+        onClick={() => setOpen(true)}
+      >
+        <CloudDownloadIcon />
+      </Button>
+
+      <CommandDialog open={open()} onOpenChange={setOpen}>
+        <CommandInput
+          placeholder={`Search models on ${BASE_DOMAIN}`}
+          disabled={searchResult()?.loading}
+          value={inputKeyword()}
+          onValueChange={setInputKeyword}
+          on:keydown={(ev) => {
+            if (ev.key === "End") {
+              ev.preventDefault()
+              ev.stopPropagation()
+              const value = ev.currentTarget.value
+              ev.currentTarget.setSelectionRange(value.length, value.length)
+              return
+            }
+
+            if (ev.key === "Home") {
+              ev.preventDefault()
+              ev.stopPropagation()
+              ev.currentTarget.setSelectionRange(0, 0)
+            }
+
+            if (ev.key === "Enter") {
+              ev.preventDefault()
+
+              if (ev.ctrlKey) {
+                searchResultContext?.startPullModel(keyword());
+                setInputKeyword("");
+              } else {
+                initiateSearch(keyword());
+              }
+            }
+          }}
+        />
+
+        <div class="flex">
+          <Button
+            variant="link"
+            size="sm"
+            class="text-xs flex gap-0"
+            onClick={() => {
+              const kw = keyword();
+              openUrl(kw.length > 0 ? `${BASE_URL}/search?` + new URLSearchParams({ q: kw }) : BASE_URL);
+            }}
+          >
+            <span class="z-[1]">Visit {BASE_DOMAIN}</span>
+              <Presence exitBeforeEnter>
+                <Show when={inputKeyword().trim()}>
+                  {(kw) => (
+                    <Show when={kw().length > 0}>
+                      <Motion.span
+                        class="-ml-[1pt]"
+                        transition={{ duration: 300 }}
+                        initial={{ opacity: 0, x: -30 }}
+                        animate={{ opacity: 1, x: 0 }}
+                        exit={{ opacity: 0, x: -30 }}
+                      >
+                        &nbsp;with "{kw()}"
+                      </Motion.span>
+                    </Show>
+                  )}
+                </Show>
+              </Presence>
+          </Button>
+        </div>
+
+        <Hints searchEntered={inputKeyword().trim().length > 0} />
+
+        <CommandList>
+          <Show when={searchResult()?.loading}>
+            <div class="px-2 py-2 text-sm">
+              <LoaderSpin text="Searching..." />
+            </div>
+          </Show>
+          <Show when={searchResult()}>
+            {(s) => (
+              <CommandGroup heading="Search result">
+              </CommandGroup>
+            )}
+          </Show>
+        </CommandList>
+      </CommandDialog>
+    </>
+  );
+}
---
+++ src/lib/components/model-page/pull-model/search-result-item.tsx
@@ -0,0 +1,102 @@
+import { SearchItem } from "~/lib/contexts/model-search-result";
+import { CommandItem } from "../../ui/command";
+import { createMemo, createSignal } from "solid-js/types/server/reactive.js";
+import { modelList } from "~/lib/contexts/globals/model-states";
+import { getTaskMap } from "~/lib/contexts/globals/pull-model-tasks";
+import { For, JSX, Match, Switch } from "solid-js";
+import { cn } from "~/lib/utils/class-names";
+import { Button } from "../../ui/button";
+import { LoaderSpin } from "../../loader-spin";
+import { CloudDownloadIcon } from "lucide-solid";
+
+interface TagBadgeProps {
+  children?: JSX.Element;
+  class?: string;
+  "on:click"?: (ev: MouseEvent) => void;
+}
+
+function TagBadge(props: TagBadgeProps) {
+  const clickHandler = (ev: MouseEvent) => props["on:click"]?.(ev);
+
+  return (
+    <span class={cn("text-xs px-2 py-1 rounded-lg", props.class)} on:click={clickHandler}>
+      {props.children}
+    </span>
+  );
+}
+
+export interface SearchResultItemProps {
+  item: SearchItem;
+}
+
+export function SearchResultItem(props: SearchResultItemProps) {
+  const LATEST = "latest";
+
+  const searchItem = () => props.item;
+
+  const [selected, setSelected] = createSignal<string>(LATEST);
+  const fullName = createMemo(() => `${props.item}:${selected()}`);
+
+  const downloadedAlready = createMemo(() => {
+    const modelName = fullName();
+    return modelList().filter(({ name }) => name === modelName).length > 0;
+  });
+  const downloading = createMemo(() => Object.keys(getTaskMap()).includes(fullName()));
+
+  return (
+    <CommandItem onClick={() => setSelected(LATEST)}>
+      <div class="flex flex-col gap-2 w-full">
+        <span class="font-bold">{searchItem().name}</span>
+        <span class="whitespace-pre-wrap flex-wrap">{searchItem().description}</span>
+      </div>
+      <div class="flex gap-2 w-full">
+        <div class="flex gap-2">
+          <For each={searchItem().tags}>
+            {(tag) => (
+              <Switch>
+                <Match when={tag.type === "category"}>
+                  <TagBadge class="bg-fuchsia-200/80 dark:bg-fuchsia-900/80">
+                    {tag.content}
+                  </TagBadge>
+                </Match>
+                <Match when={tag.type === "parameter"}>
+                  <TagBadge
+                    class={cn(
+                      "bg-sky-200/80 dark:bg-sky-900/80 hover:bg-sky-300 dark:hover:bg-sky-800 cursor-pointer",
+                      selected() === tag.content && "bg-sky-400 dark:bg-sky-700",
+                    )}
+                    on:click={(ev) => {
+                      ev.stopPropagation();
+                      setSelected(tag.content);
+                    }}
+                  >
+                    {tag.content}
+                  </TagBadge>
+                </Match>
+              </Switch>
+            )}
+          </For>
+        </div>
+        <div class="grow" />
+        <div class="flex gap-2 lg:gap-4 place-content-end text-xs items-center">
+          <span>Selected: {selected()}</span>
+          <Button
+            size="icon"
+            variant="ghost"
+            on:click={(ev) => {
+              ev.stopPropagation();
+            }}
+          >
+            <Switch fallback={(
+              <CloudDownloadIcon class={cn("size-4", downloadedAlready() && "text-muted-foreground")} />
+            )}>
+              <Match when={downloading()}>
+                <LoaderSpin class="size-4" />
+              </Match>
+            </Switch>
+          </Button>
+        </div>
+      </div>
+    </CommandItem>
+  );
+}
