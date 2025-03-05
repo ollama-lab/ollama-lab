d912a716b2b4bef4c82d19b9b2aa8a7221021036
Charles Dong <chardon_cs@proton.me>
2025-03-05 14:11:47 +0800

    Update system prompt toggle

--- src/lib/components/chat-sessions/chat-panel/header/system-prompt-toggle.tsx
+++ src/lib/components/chat-sessions/chat-panel/header/system-prompt-toggle.tsx
@@ -5,6 +5,7 @@ import { getModelSystemPrompt } from "~/lib/commands/system-prompts";
 import { LoaderSpin } from "~/lib/components/loader-spin";
 import { SwitchButton, SwitchField } from "~/lib/components/ui/switch";
 import { getChatHistory } from "~/lib/contexts/globals/chat-history";
+import { getInputPrompt, setInputPrompt } from "~/lib/contexts/globals/prompt-input";
 import { getSessionWiseModel } from "~/lib/contexts/globals/session-wise-model";
 
 export function SystemPromptToggle() {
@@ -21,7 +22,11 @@ export function SystemPromptToggle() {
     <div class="px-2">
       <Suspense fallback={<LoaderSpin class="size-4" />}>
         <Show when={!getChatHistory() && !!systemPrompt()}>
-          <SwitchField name="use-system-prompt">
+          <SwitchField
+            name="use-system-prompt"
+            checked={getInputPrompt().useSystemPrompt}
+            onChange={(value) => setInputPrompt("useSystemPrompt", value)}
+          >
             <SwitchButton />
             <SwitchLabel>Use system prompt</SwitchLabel>
           </SwitchField>
---
+++ src/lib/contexts/globals/prompt-input.ts
@@ -0,0 +1,27 @@
+import { createStore } from "solid-js/store";
+import { createMemo, createSignal } from "solid-js/types/server/reactive.js";
+import { IncomingUserPrompt } from "~/lib/models/chat";
+import { getSessionWiseModel } from "./session-wise-model";
+
+const [inputPrompt, setInputPrompt] = createStore<IncomingUserPrompt>({ text: "" });
+const [hidePromptBar, setHidePromptBar] = createSignal(false);
+
+export const isSubmittable = createMemo(() => {
+  if (!getSessionWiseModel()) {
+    return false;
+  }
+
+  if (inputPrompt.text.length > 0) {
+    return true;
+  }
+
+  if (inputPrompt.imagePaths && inputPrompt.imagePaths.length > 0) {
+    return true;
+  }
+
+  return false;
+});
+
+export const getInputPrompt = () => inputPrompt;
+
+export { setInputPrompt, hidePromptBar, setHidePromptBar };
