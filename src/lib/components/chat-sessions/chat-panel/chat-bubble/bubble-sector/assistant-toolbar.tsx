4171566d72a2a9301e439eb12e71a58c83625caa
Charles Dong <chardon_cs@proton.me>
2025-03-07 19:36:27 +0800

    Add success toast for copying

--- src/lib/components/chat-sessions/chat-panel/chat-bubble/bubble-sector/assistant-toolbar.tsx
+++ src/lib/components/chat-sessions/chat-panel/chat-bubble/bubble-sector/assistant-toolbar.tsx
@@ -2,6 +2,7 @@ import { CopyIcon } from "lucide-solid";
 import { Show } from "solid-js";
 import { useChatEntry } from "~/lib/contexts/chat-entry";
 import { RegenDropdown } from "../regen-dropdown";
+import { toast } from "solid-sonner";
 
 export function AssistantBubbleSectorFooterToolbar() {
   const chat = useChatEntry();
@@ -15,6 +16,7 @@ export function AssistantBubbleSectorFooterToolbar() {
             title="Copy Markdown"
             onClick={() => {
               navigator.clipboard.writeText(c());
+              toast.success("Successfully copied.");
             }}
           >
             <CopyIcon class="size-4" />
