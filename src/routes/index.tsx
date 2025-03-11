46f90aabda5103081a128ec6bd9973592b21dd14
Charles Dong <chardon_cs@proton.me>
2025-03-11 08:52:31 +0800

    Add agent list

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,3 +1,4 @@
+import { AgentList } from "~/lib/components/agent-list";
 import { ChatPanel } from "~/lib/components/chat-sessions/chat-panel";
 import { SessionList } from "~/lib/components/chat-sessions/session-list";
 import { Resizable, ResizableHandle, ResizablePanel } from "~/lib/components/ui/resizable";
@@ -6,7 +7,14 @@ export default function IndexPage() {
   return (
     <Resizable orientation="horizontal">
       <ResizablePanel initialSize={0.25} collapsible>
-        <SessionList />
+        <Resizable orientation="vertical">
+          <ResizablePanel initialSize={0.75} collapsible>
+            <SessionList />
+          </ResizablePanel>
+          <ResizablePanel initialSize={0.25} collapsible>
+            <AgentList />
+          </ResizablePanel>
+        </Resizable>
       </ResizablePanel>
       <ResizableHandle />
       <ResizablePanel initialSize={0.75}>
