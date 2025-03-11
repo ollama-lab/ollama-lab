b683273eba2085c542232fc8ef7b347381e2eb57
Charles Dong <chardon_cs@proton.me>
2025-03-11 10:37:29 +0800

    Add operations to contexts

--- src/lib/components/agent-list/index.tsx
+++ src/lib/components/agent-list/index.tsx
@@ -1,9 +1,16 @@
-import { Component } from "solid-js";
+import { Component, For, onMount } from "solid-js";
 import { HeaderBar } from "../custom-ui/header-bar";
 import { Button } from "../ui/button";
 import { PlusIcon } from "lucide-solid";
+import { getAgentList, reloadAgentList } from "~/lib/contexts/globals/agents";
+import { toast } from "solid-sonner";
+import { AgentListItem } from "./item";
 
 export const AgentList: Component = () => {
+  onMount(() => {
+    reloadAgentList().catch((err) => toast.error(String(err)));
+  });
+
   return (
     <div class="w-full h-full flex flex-col">
       <HeaderBar title="Agents">
@@ -17,6 +24,11 @@ export const AgentList: Component = () => {
       </HeaderBar>
 
       <div class="flex flex-col grow">
+        <For each={getAgentList()}>
+          {(agent) => (
+            <AgentListItem agent={agent} />
+          )}
+        </For>
       </div>
     </div>
   );
--- src/lib/contexts/globals/agents.ts
+++ src/lib/contexts/globals/agents.ts
@@ -1,6 +1,6 @@
-import { createStore } from "solid-js/store";
-import { getAllAgents } from "~/lib/commands/agents";
-import { Agent } from "~/lib/models/agent";
+import { createStore, produce, reconcile } from "solid-js/store";
+import { getAllAgents, addAgent as addAgent_, updateAgent as updateAgent_, deleteAgent as deleteAgent_ } from "~/lib/commands/agents";
+import { Agent, AgentCreation, AgentUpdate } from "~/lib/models/agent";
 
 const [agentList, setAgentList] = createStore<Agent[]>([]);
 
@@ -10,4 +10,34 @@ export function getAgentList() {
 
 export async function reloadAgentList() {
   setAgentList(await getAllAgents());
+}
+
+export async function addAgent(createInfo: AgentCreation) {
+  setAgentList(agentList.length, await addAgent_(createInfo.model));
+}
+
+export async function updateAgent(id: number, updateInfo: AgentUpdate) {
+  const index = agentList.findIndex((item) => item.id === id);
+  if (index < 0) {
+    return;
+  }
+
+  const newValue = await updateAgent_(id, updateInfo);
+  if (newValue) {
+    setAgentList(index, reconcile(newValue));
+  }
+}
+
+export async function deleteAgent(id: number) {
+  const index = agentList.findIndex((item) => item.id === id);
+  if (index < 0) {
+    return;
+  }
+
+  const deletedId = await deleteAgent_(id);
+  if (deletedId !== null) {
+    setAgentList(produce((list) => {
+      list.splice(index, 1);
+    }));
+  }
 }
