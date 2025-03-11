6692b168fd416d52e5c108c06037a5aad3504e0a
Charles Dong <chardon_cs@proton.me>
2025-03-11 11:17:46 +0800

    Add sidebar switching

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,22 +1,35 @@
-import { Component } from "solid-js";
+import { Component, Match, Switch } from "solid-js";
 import { AgentList } from "~/lib/components/agent-list";
 import { ChatPanel } from "~/lib/components/chat-sessions/chat-panel";
 import { SessionList } from "~/lib/components/chat-sessions/session-list";
 import { Resizable, ResizableHandle, ResizablePanel } from "~/lib/components/ui/resizable";
+import { getCurrentSettings } from "~/lib/contexts/globals/settings";
 
 const IndexPage: Component = () => {
+  const NormalSidebar = () => (
+    <SessionList />
+  );
+
+  const H2hEnabledSidebar = () => (
+    <Resizable orientation="vertical">
+      <ResizablePanel initialSize={0.75} collapsible class="overflow-hidden">
+        <SessionList />
+      </ResizablePanel>
+      <ResizableHandle />
+      <ResizablePanel initialSize={0.25} collapsible class="overflow-hidden">
+        <AgentList />
+      </ResizablePanel>
+    </Resizable>
+  );
+
   return (
     <Resizable orientation="horizontal">
       <ResizablePanel initialSize={0.25} collapsible class="overflow-hidden">
-        <Resizable orientation="vertical">
-          <ResizablePanel initialSize={0.75} collapsible class="overflow-hidden">
-            <SessionList />
-          </ResizablePanel>
-          <ResizableHandle />
-          <ResizablePanel initialSize={0.25} collapsible class="overflow-hidden">
-            <AgentList />
-          </ResizablePanel>
-        </Resizable>
+        <Switch fallback={<NormalSidebar />}>
+          <Match when={getCurrentSettings().h2h ?? false}>
+            <H2hEnabledSidebar />
+          </Match>
+        </Switch>
       </ResizablePanel>
       <ResizableHandle />
       <ResizablePanel initialSize={0.75} class="overflow-hidden">
