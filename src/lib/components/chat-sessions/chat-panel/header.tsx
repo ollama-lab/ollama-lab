80f44a1970b8a99f77140dcdf3e31ea0853a1116
Charles Dong <chardon_cs@proton.me>
2025-03-21 11:00:56 +0800

    Remove session system prompt from normal mode header bar

--- src/lib/components/chat-sessions/chat-panel/header.tsx
+++ src/lib/components/chat-sessions/chat-panel/header.tsx
@@ -1,54 +1,11 @@
-import { Component, createMemo, Show } from "solid-js";
+import { Component } from "solid-js";
 import { ModelSelector } from "../../custom-ui/model-selector";
-import { SystemPromptToggle } from "./header/system-prompt-toggle";
-import { getCurrentSettings } from "~/lib/contexts/globals/settings";
-import { Dialog, DialogContent, DialogTitle, DialogTrigger } from "../../ui/dialog";
-import { FileSlidersIcon } from "lucide-solid";
-import { Button } from "../../ui/button";
-import { TextField, TextFieldTextArea } from "../../ui/text-field";
-import { getCandidateSessionSystemPrompt, setCandidateSessionSystemPrompt } from "~/lib/contexts/globals/candidate-session-system-prompt";
 
 export const ChatPanelHeader: Component = () => {
-  const systemPrompt = createMemo(() => getCandidateSessionSystemPrompt());
-
-  const updateSystemPrompt = async (value: string) => {
-    await setCandidateSessionSystemPrompt(value);
-  };
-
   return (
     <div class="sticky shrink-0 border-b border-border h-12 flex items-center backdrop-blur-lg bg-background/50">
       <div class="grow flex gap-2">
         <ModelSelector />
-      </div>
-      <div class="flex gap-2 px-3.5 items-center">
-        <SystemPromptToggle />
-        <Show when={getCurrentSettings().h2h ?? false}>
-          <span class="text-sm">
-            Head-to-head mode
-          </span>
-
-          <Dialog>
-            <DialogTrigger as={Button} variant="outline" size="icon">
-              <FileSlidersIcon class="size-4" />
-            </DialogTrigger>
-
-            <DialogContent>
-              <DialogTitle>Session system prompt</DialogTitle>
-              <div>
-                <TextField
-                  name="system-prompt"
-                  defaultValue={systemPrompt()}
-                >
-                  <TextFieldTextArea
-                    onBlur={(ev) => {
-                      updateSystemPrompt(ev.currentTarget.value);
-                    }}
-                  />
-                </TextField>
-              </div>
-            </DialogContent>
-          </Dialog>
-        </Show>
       </div>
     </div>
   );
