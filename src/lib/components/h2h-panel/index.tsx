af1517de7f8dfb9b2d97cd996685c04ee11d6e12
Charles Dong <chardon_cs@proton.me>
2025-03-24 11:50:39 +0800

    Switch to effect

--- src/lib/components/h2h-panel/index.tsx
+++ src/lib/components/h2h-panel/index.tsx
@@ -1,4 +1,4 @@
-import { Component, createMemo, createSignal, Show } from "solid-js";
+import { Component, createEffect, createMemo, createSignal, Show } from "solid-js";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "../ui/tabs";
 import { ChatFeeds } from "../chat-sessions/chat-panel/chat-feeds";
 import { PromptInput } from "../chat-sessions/prompt-input";
@@ -11,17 +11,23 @@ import { currentSession } from "~/lib/contexts/globals/current-session";
 const [tabValue, setTabValue] = createSignal("chats");
 
 const H2hPanel: Component = () => {
-  const derivedTabValue = createMemo(() => currentSession("h2h") ? tabValue() : "agents");
+  const currentH2hSession = createMemo(() => currentSession("h2h"));
+
+  createEffect(() => {
+    if (!currentH2hSession() && tabValue() === "chats") {
+      setTabValue("agents");
+    }
+  });
 
   return (
-    <Tabs class="px-2 py-2 h-full flex flex-col" value={derivedTabValue()} onChange={setTabValue}>
-        <Show when={currentSession("h2h")}>
+    <Tabs class="px-2 py-2 h-full flex flex-col" value={tabValue()} onChange={setTabValue}>
+        <Show when={currentH2hSession()}>
           <TabsList class="grid w-full grid-cols-2">
             <TabsTrigger value="chats">Chats</TabsTrigger>
             <TabsTrigger value="agents">Agents</TabsTrigger>
           </TabsList>
       </Show>
-      <Show when={currentSession("h2h")}>
+      <Show when={currentH2hSession()}>
         <TabsContent value="chats" class="flex flex-col grow">
           <div class="grow px-4 py-2 w-full overflow-y-auto">
             <ChatFeeds mode="h2h" />
