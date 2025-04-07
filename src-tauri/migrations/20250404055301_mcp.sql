b1e83bc332c6a1a8e5167b3cc49bd9f3c3310c13
Charles Dong <chardon_cs@proton.me>
2025-04-07 09:01:06 +0800

    Add default MCP

--- src-tauri/migrations/20250404055301_mcp.sql
+++ src-tauri/migrations/20250404055301_mcp.sql
@@ -14,3 +14,11 @@ CREATE TABLE mcp_sources (
     session_id      INTEGER NOT NULL REFERENCES sessions (id) ON DELETE CASCADE ON UPDATE CASCADE,
     date_created    INTEGER NOT NULL DEFAULT (unixepoch())
 );
+
+CREATE TABLE default_mcp (
+    id              INTEGER NOT NULL PRIMARY KEY,
+    label           TEXT,
+    source          TEXT NOT NULL,
+    transport_type  TEXT NOT NULL REFERENCES mcp_transport_types (name) ON UPDATE CASCADE,
+    profile_id      INTEGER NOT NULL REFERENCES profiles (id) ON DELETE CASCADE ON UPDATE CASCADE
+);
