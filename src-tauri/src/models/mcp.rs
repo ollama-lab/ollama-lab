411a62c1945f01d194442215bf2a6a76dda805af
Charles Dong <chardon_cs@proton.me>
2025-04-17 18:32:03 +0800

    Add MCP source display logic

--- src-tauri/src/models/mcp.rs
+++ src-tauri/src/models/mcp.rs
@@ -3,7 +3,7 @@ use serde::{Deserialize, Serialize};
 use sqlx::prelude::FromRow;
 use transport_type::TransportType;
 
-use crate::{errors::Error, utils::crud::{Create, Delete, Update}};
+use crate::{errors::Error, utils::crud::{Create, Delete, ListAll, Update}};
 
 pub mod transport_type;
 
@@ -30,6 +30,101 @@ pub struct McpSourceUpdate<'a, 'b> {
     pub label: Option<&'a str>,
     pub source: Option<&'b str>,
     pub transport_type: Option<TransportType>,
+}
+
+impl McpSource {
+    pub async fn list_enabled<'a>(
+        executor: impl sqlx::Executor<'a, Database = sqlx::Sqlite>,
+        session_id: i64,
+    ) -> Result<Vec<Self>, Error> {
+        let ret = sqlx::query_as::<_, Self>(r#"
+            SELECT m.id, m.label, m.source, m.transport_type, m.profile_id, m.data_created
+            FROM mcp_sources m
+            INNER JOIN session_enabled_mcp_sources sems
+            ON m.id = sems.mcp_source_id
+            WHERE sems.session_id = $1;
+        "#)
+        .bind(session_id)
+        .fetch_all(executor)
+        .await?;
+
+        Ok(ret)
+    }
+
+    pub async fn list_enabled_id<'a>(
+        executor: impl sqlx::Executor<'a, Database = sqlx::Sqlite>,
+        session_id: i64,
+    ) -> Result<Vec<i64>, Error> {
+        let ret = sqlx::query_as::<_, (i64,)>(r#"
+            SELECT mcp_source_id
+            FROM session_enabled_mcp_sources
+            WHERE session_id = $1;
+        "#)
+        .bind(session_id)
+        .fetch_all(executor)
+        .await?
+        .into_iter()
+        .map(|(session_id,)| session_id)
+        .collect();
+
+        Ok(ret)
+    }
+
+    pub async fn enable<'a>(
+        executor: impl sqlx::Executor<'a, Database = sqlx::Sqlite>,
+        mcp_source_id: i64,
+        session_id: i64,
+    ) -> Result<Option<i64>, Error> {
+        let ret = sqlx::query_as::<_, (i64,)>(r#"
+            INSERT INTO session_enabled_mcp_sources (session_id, mcp_source_id)
+            VALUES ($1, $2)
+            RETURNING mcp_source_id;
+        "#)
+        .bind(session_id)
+        .bind(mcp_source_id)
+        .fetch_optional(executor)
+        .await?
+        .map(|(id,)| id);
+
+        Ok(ret)
+    }
+
+    pub async fn disable<'a>(
+        executor: impl sqlx::Executor<'a, Database = sqlx::Sqlite>,
+        mcp_source_id: i64,
+        session_id: i64,
+    ) -> Result<(), Error> {
+        sqlx::query(r#"
+            DELETE FROM session_enabled_mcp_sources
+            WHERE session_id = $1, mcp_source_id = $2;
+        "#)
+        .bind(session_id)
+        .bind(mcp_source_id)
+        .execute(executor)
+        .await?;
+
+        Ok(())
+    }
+}
+
+impl ListAll for McpSource {
+    type Selector<'t> = i64;
+
+    async fn list_all<'a>(
+        executor: impl sqlx::Executor<'a, Database = sqlx::Sqlite>,
+        profile_id: Self::Selector<'_>,
+    ) -> Result<Vec<Self>, Error> {
+        let ret = sqlx::query_as::<_, Self>(r#"
+            SELECT id, label, source, transport_type, profile_id, date_created
+            FROM mcp_sources
+            WHERE profile_id = $1;
+        "#)
+        .bind(profile_id)
+        .fetch_all(executor)
+        .await?;
+
+        Ok(ret)
+    }
 }
 
 impl Create for McpSource {
